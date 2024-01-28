059e559a46d11df90835589f8a79205aa3318aa1
Saumya <patelsaumya767@gmail.com>
2024-01-28 17:56:50 +0400

    Convert the app to a menu bar only app

--- Macboard/MacboardApp.swift
+++ Macboard/MacboardApp.swift
@@ -1,14 +1,47 @@
 import SwiftUI
 import SwiftData
+import Cocoa
 
 @main
 struct MacboardApp: App {
+    
+    @NSApplicationDelegateAdaptor(AppDelegate.self) private var appDelegate
+    
     var body: some Scene {
-        WindowGroup {
-            ClipboardItemListView()
-                .frame(minWidth: 700, idealWidth: 700, maxWidth: 700, minHeight: 500, idealHeight: 500, maxHeight: 500)
+        Settings {
+            EmptyView()
         }
-        .modelContainer(for: [ClipboardItem.self])
-        .windowResizability(.contentSize)
     }
+}
+
+class AppDelegate: NSObject, NSApplicationDelegate, ObservableObject {
+    private var statusItem: NSStatusItem!
+    private var popover: NSPopover!
+    
+    @MainActor func applicationDidFinishLaunching(_ notification: Notification) {
+        let rootView = ClipboardItemListView().modelContainer(for: [ClipboardItem.self])
+        statusItem = NSStatusBar.system.statusItem(withLength: NSStatusItem.variableLength)
+        
+        if let statusButton = statusItem.button {
+            statusButton.image = NSImage(systemSymbolName: "list.bullet.clipboard", accessibilityDescription: "Macboard")
+            statusButton.action = #selector(togglePopover)
+        }
+        
+        self.popover = NSPopover()
+        self.popover.contentSize = NSSize(width: 700, height: 500)
+        self.popover.behavior = .transient
+        self.popover.contentViewController = NSHostingController(rootView: rootView)
+    }
+    
+    @objc func togglePopover() {
+        if let button = statusItem.button {
+            if popover.isShown {
+                self.popover.performClose(nil)
+            } else {
+                popover.show(relativeTo: button.bounds, of: button, preferredEdge: NSRectEdge.minY)
+            }
+        }
+        
+    }
+    
 }
