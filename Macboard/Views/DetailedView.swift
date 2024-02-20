54b95bf646ed8eda8e013fbf058d632892d1d4dc
Saumya <patelsaumya767@gmail.com>
2024-02-20 15:33:37 +0530

    Add support for macOS 12

--- Macboard/Views/DetailedView.swift
+++ Macboard/Views/DetailedView.swift
@@ -3,21 +3,31 @@ import SwiftUI
 struct DetailedView: View {
     
     let clipboardItem: ClipboardItem
+    
     @ObservedObject var vm: MetadataViewModel
+    @Binding var selectedItem: ClipboardItem?
     
     @State private var hover: Bool = false
     
     var body: some View {
-        VStack {
-            if clipboardItem.contentType == "Text" {
-                List {
-                    Section {
-                        if clipboardItem.content!.isValidURL {
-                            let imageURLString = vm.metadata.first(where: {$0.key == "Image"})?.value
-                            if imageURLString != nil {
-                                if imageURLString != "Not Found" {
-                                    let imageURL = URL(string: imageURLString!)!
-                                    RemoteImage(url: imageURL)
+        if selectedItem != nil {
+            VStack {
+                if clipboardItem.contentType == "Text" {
+                    List {
+                        Section {
+                            if clipboardItem.content!.isValidURL {
+                                let imageURLString = vm.metadata.first(where: {$0.key == "Image"})?.value
+                                if imageURLString != nil {
+                                    if imageURLString != "Not Found" {
+                                        let imageURL = URL(string: imageURLString!)!
+                                        RemoteImage(url: imageURL)
+                                    } else {
+                                        Image(systemName: "photo.fill")
+                                            .resizable()
+                                            .aspectRatio(contentMode: .fill)
+                                            .scaledToFit()
+                                            .frame(maxWidth: .infinity, alignment: .center)
+                                    }
                                 } else {
                                     Image(systemName: "photo.fill")
                                         .resizable()
@@ -25,101 +35,148 @@ struct DetailedView: View {
                                         .scaledToFit()
                                         .frame(maxWidth: .infinity, alignment: .center)
                                 }
+                                
                             } else {
-                                Image(systemName: "photo.fill")
-                                    .resizable()
-                                    .aspectRatio(contentMode: .fill)
-                                    .scaledToFit()
-                                    .frame(maxWidth: .infinity, alignment: .center)
-                            }
-                            
-                        } else {
-                            Text(clipboardItem.content!)
-                                .textSelection(.enabled)
-                                .onHover(perform: { isHovering in
-                                    self.hover = isHovering
-                                    DispatchQueue.main.async {
-                                        if self.hover {
-                                            NSCursor.iBeam.push()
-                                        } else {
-                                            NSCursor.pop()
-                                        }
-                                    }
-                                })
-                        }
-                    } header: {
-                        HStack {
-                            if clipboardItem.content!.isValidURL {
-                                Image(systemName: "photo.fill")
-                                Text("Meta Image")
-                            } else {
-                                Image(systemName: "doc.plaintext.fill")
-                                Text("Complete Text")
-                            }
-                        }
-                    }
-                    
-                    if clipboardItem.content!.isValidURL {
-                        Section {
-                            HStack {
-                                Image(systemName: "person.badge.clock.fill")
-                                Text("Copied:")
-                                Spacer()
-                                Text(clipboardItem.createdAt!.timeAgoDisplay())
-                            }
-                            
-                            HStack {
-                                Image(systemName: "link")
-                                Text("URL:")
-                                Spacer()
-                                Link(clipboardItem.content!, destination: URL(string: clipboardItem.content!)!)
-                                    .textFieldStyle(.roundedBorder)
+                                Text(clipboardItem.content!)
                                     .textSelection(.enabled)
                                     .onHover(perform: { isHovering in
                                         self.hover = isHovering
                                         DispatchQueue.main.async {
                                             if self.hover {
-                                                NSCursor.pointingHand.push()
+                                                NSCursor.iBeam.push()
                                             } else {
                                                 NSCursor.pop()
                                             }
                                         }
                                     })
                             }
-                            
-                            if let title = vm.metadata.first(where: {$0.key == "Title"})?.value {
-                                HStack {
-                                    Image(systemName: "pencil")
-                                    Text("Title:")
-                                    Spacer()
-                                    Text(title)
-                                }
-                            }
-                            
-                            if let description = vm.metadata.first(where: {$0.key == "Description"})?.value {
-                                HStack {
-                                    Image(systemName: "note.text")
-                                    Text("Description:")
-                                    Spacer()
-                                    Text(description)
-                                }
-                            }
-                            
-                            HStack {
-                                Image(systemName: "app.badge.checkmark.fill")
-                                Text("Source App:")
-                                Spacer()
-                                Text(clipboardItem.sourceApp ?? "Unknown")
-                            }
-                            
                         } header: {
                             HStack {
-                                Image(systemName: "info.circle.fill")
-                                Text("URL Details")
+                                if clipboardItem.content!.isValidURL {
+                                    Image(systemName: "photo.fill")
+                                    Text("Meta Image")
+                                } else {
+                                    Image(systemName: "doc.plaintext.fill")
+                                    Text("Complete Text")
+                                }
                             }
                         }
                         
-                    } else {
+                        if clipboardItem.content!.isValidURL {
+                            Section {
+                                HStack {
+                                    Image(systemName: "person.badge.clock.fill")
+                                    Text("Copied:")
+                                    Spacer()
+                                    Text(clipboardItem.createdAt!.timeAgoDisplay())
+                                }
+                                
+                                HStack {
+                                    Image(systemName: "link")
+                                    Text("URL:")
+                                    Spacer()
+                                    Link(clipboardItem.content!, destination: URL(string: clipboardItem.content!)!)
+                                        .textFieldStyle(.roundedBorder)
+                                        .textSelection(.enabled)
+                                        .onHover(perform: { isHovering in
+                                            self.hover = isHovering
+                                            DispatchQueue.main.async {
+                                                if self.hover {
+                                                    NSCursor.pointingHand.push()
+                                                } else {
+                                                    NSCursor.pop()
+                                                }
+                                            }
+                                        })
+                                }
+                                
+                                if let title = vm.metadata.first(where: {$0.key == "Title"})?.value {
+                                    HStack {
+                                        Image(systemName: "pencil")
+                                        Text("Title:")
+                                        Spacer()
+                                        Text(title)
+                                    }
+                                }
+                                
+                                if let description = vm.metadata.first(where: {$0.key == "Description"})?.value {
+                                    HStack {
+                                        Image(systemName: "note.text")
+                                        Text("Description:")
+                                        Spacer()
+                                        Text(description)
+                                    }
+                                }
+                                
+                                HStack {
+                                    Image(systemName: "app.badge.checkmark.fill")
+                                    Text("Source App:")
+                                    Spacer()
+                                    Text(clipboardItem.sourceApp ?? "Unknown")
+                                }
+                                
+                            } header: {
+                                HStack {
+                                    Image(systemName: "info.circle.fill")
+                                    Text("URL Details")
+                                }
+                            }
+                            
+                        } else {
+                            Section {
+                                HStack {
+                                    Image(systemName: "person.badge.clock.fill")
+                                    Text("Copied:")
+                                    Spacer()
+                                    Text(clipboardItem.createdAt!.timeAgoDisplay())
+                                }
+                                
+                                HStack {
+                                    Image(systemName: "note.text")
+                                    Text("Type:")
+                                    Spacer()
+                                    if clipboardItem.content!.contains("\n") {
+                                        Text("Multi-line Text")
+                                    } else if clipboardItem.content!.isNum {
+                                        Text("Number")
+                                    } else {
+                                        Text("RTF - Rich Text Format")
+                                    }
+                                }
+                                
+                                HStack {
+                                    Image(systemName: "app.badge.checkmark.fill")
+                                    Text("Source App:")
+                                    Spacer()
+                                    Text(clipboardItem.sourceApp ?? "Unknown")
+                                }
+                                
+                            } header: {
+                                HStack {
+                                    Image(systemName: "info.circle.fill")
+                                    Text("Details")
+                                }
+                            }
+                        }
+                    }
+                    .padding(.bottom, -8)
+                    
+                } else {
+                    List {
+                        Section {
+                            let image = dataToImage(clipboardItem.imageData!)
+                            image.0
+                                .resizable()
+                                .aspectRatio(contentMode: .fill)
+                                .scaledToFit()
+                                .frame(maxWidth: .infinity, alignment: .center)
+                        } header: {
+                            HStack {
+                                Image(systemName: "photo.fill")
+                                Text("Image")
+                            }
+                        }
+                        
                         Section {
                             HStack {
                                 Image(systemName: "person.badge.clock.fill")
@@ -129,16 +186,10 @@ struct DetailedView: View {
                             }
                             
                             HStack {
-                                Image(systemName: "note.text")
+                                Image(systemName: "photo.fill")
                                 Text("Type:")
                                 Spacer()
-                                if clipboardItem.content!.contains("\n") {
-                                    Text("Multi-line Text")
-                                } else if clipboardItem.content!.isNum {
-                                    Text("Number")
-                                } else {
-                                    Text("RTF - Rich Text Format")
-                                }
+                                Text("TIFF Image")
                             }
                             
                             HStack {
@@ -155,54 +206,55 @@ struct DetailedView: View {
                             }
                         }
                     }
+                    .padding(.bottom, -8)
                 }
+                Divider()
                 
-            } else {
-                List {
-                    Section {
-                        let image = dataToImage(clipboardItem.imageData!)
-                        image.0
-                            .resizable()
-                            .aspectRatio(contentMode: .fill)
-                            .scaledToFit()
-                            .frame(maxWidth: .infinity, alignment: .center)
-                    } header: {
-                        HStack {
-                            Image(systemName: "photo.fill")
-                            Text("Image")
-                        }
-                    }
-                    
-                    Section {
-                        HStack {
-                            Image(systemName: "person.badge.clock.fill")
-                            Text("Copied:")
-                            Spacer()
-                            Text(clipboardItem.createdAt!.timeAgoDisplay())
-                        }
-                        
-                        HStack {
-                            Image(systemName: "photo.fill")
-                            Text("Type:")
-                            Spacer()
-                            Text("TIFF Image")
-                        }
-                        
-                        HStack {
-                            Image(systemName: "app.badge.checkmark.fill")
-                            Text("Source App:")
-                            Spacer()
-                            Text(clipboardItem.sourceApp ?? "Unknown")
-                        }
-                        
-                    } header: {
-                        HStack {
-                            Image(systemName: "info.circle.fill")
-                            Text("Details")
-                        }
-                    }
+                HStack {
+                    Spacer()
+                    Text("Copy & Hide")
+                        .padding(.top, -10)
+                    Image(systemName: "return")
+                        .opacity(0.7)
+                        .padding(.top, -8)
+                        .padding(.trailing, 4)
+                    Text("|")
+                        .font(.largeTitle)
+                        .opacity(0.3)
+                        .padding(.top, -14)
+                        .padding(.leading, -2)
+                        .padding(.trailing, 2)
+                    Text("Copy")
+                        .padding(.top, -10)
+                    Image(systemName: "command")
+                        .opacity(0.7)
+                        .padding(.top, -9)
+                        .padding(.trailing, -2)
+                    Text("C")
+                        .opacity(0.7)
+                        .padding(.top, -9.5)
+                        .padding(.trailing, 4)
+                    Text("|")
+                        .font(.largeTitle)
+                        .opacity(0.3)
+                        .padding(.top, -14)
+                        .padding(.trailing, 2)
+                    Text("Delete")
+                        .padding(.top, -10)
+                    Image(systemName: "command")
+                        .opacity(0.7)
+                        .padding(.top, -10)
+                        .padding(.trailing, -2)
+                    Text("D")
+                        .opacity(0.7)
+                        .padding(.top, -10)
+                        .padding(.trailing, 10)
                 }
             }
+        } else {
+            Text("Select an item to get its detailed view")
+                .bold()
+                .padding()
         }
     }
 }
