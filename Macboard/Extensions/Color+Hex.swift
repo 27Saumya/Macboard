3ff3f795ecdba49bea95180a273d5554a3f91762
Saumya <64534496+27Saumya@users.noreply.github.com>
2024-03-15 18:20:58 +0530

    Add support for color(s)

---
+++ Macboard/Extensions/Color+Hex.swift
@@ -0,0 +1,12 @@
+import SwiftUI
+
+extension Color {
+    init(hex: String) {
+        let hex = hex.trimmingCharacters(in: CharacterSet(charactersIn: "#"))
+        let rgbValue = UInt32(hex, radix: 16)
+        let r = Double((rgbValue! & 0xFF0000) >> 16) / 255
+        let g = Double((rgbValue! & 0x00FF00) >> 8) / 255
+        let b = Double(rgbValue! & 0x0000FF) / 255
+        self.init(red: r, green: g, blue: b)
+    }
+}
